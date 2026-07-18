4cbb950c74388895c2666e014edda170ba460f67
emmmilla <emmmilla@student.42madrid.com>
2026-07-18 13:07:55 +0200

    Adding Radix v1.0 tested

--- src/core/complex/sort_radix.c
+++ src/core/complex/sort_radix.c
@@ -3,19 +3,65 @@
 /*                                                        :::      ::::::::   */
 /*   sort_radix.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: durisosa <durisosa@student.42madrid.com    +#+  +:+       +#+        */
+/*   By: emmmilla <emmmilla@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/15 15:40:14 by durisosa          #+#    #+#             */
-/*   Updated: 2026/07/17 14:55:26 by durisosa         ###   ########.fr       */
+/*   Updated: 2026/07/18 13:07:33 by emmmilla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-/**
- * RADIX
- */
+int	get_max_bits(t_stack *stack)
+{
+	int	max_index;
+	int	bits;
+
+	max_index = stack->size - 1;
+	bits = 1;
+	while (max_index > 1)
+	{
+		max_index >>= 1;
+		bits++;
+	}
+	return (bits);
+}
+
+static void	push_back_to_a(t_stack *a, t_stack *b)
+{
+	while (b->size > 0)
+		pa(a, b);
+}
+
+static void	process_bit(t_stack *a, t_stack *b, int bit)
+{
+	int	size;
+
+	size = a->size;
+	while (size > 0)
+	{
+		if (((a->top->index >> bit) & 1) == 0)
+			pb(a, b);
+		else
+			ra(a);
+		size--;
+	}
+}
+
 void	sort_radix(t_stack *a, t_stack *b)
 {
+	int	bit;
+	int	max_bits;
 
+	if (stack_is_sorted(a))
+		return ;
+	max_bits = get_max_bits(a);
+	bit = 0;
+	while (bit < max_bits)
+	{
+		process_bit(a, b, bit);
+		push_back_to_a(a, b);
+		bit++;
+	}
 }
+
